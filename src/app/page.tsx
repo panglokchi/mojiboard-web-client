c3b6601efcf6946b8dccf84e51b9fb17e222af64
panglokchi <lokchi.pang@gmail.com>
2025-04-22 00:42:49 +0100

    added new post input box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,318 @@
+'use client'
 import Image from "next/image";
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [textboxExpanded, setTextboxExpanded] = useState(false);
+  const [textboxInput, setTextboxInput] = useState('');
+  const [textboxValid, setTextboxValid] = useState(true);
+  const [showComments, setShowComments] = useState<number|null>(null)
+  const [commentInput, setCommentInput] = useState('');
+  const [comments, setComments] = useState<any[]|null>(null)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const updateMessages = () => {
+    fetch('http://172.26.87.217:8080/message/top')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setData(data);
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    updateMessages();
+  }, []);
+
+  const handleExpandTextBox = () => {
+    setTextboxExpanded(!textboxExpanded);
+  };
+
+  const handleSubmit = async (e: React.ChangeEvent<any>) => {
+    e.preventDefault();
+    try {
+      const value = textboxInput;
+      // Regular expression to check for emoji characters
+      const emojiRegex = /[\p{Emoji}]/u;
+
+      console.log(value)
+      // Validate input: only allow emojis
+      if (emojiRegex.test(value) || value === '') {
+
+      } else {
+        console.log("Only emojis")
+        return;
+      }
+      
+
+      const lines = textboxInput.split('\n');
+      const title = lines[0]; // First line as title
+      const content = lines.slice(1).join('\n'); // Rest as contents
+      const time = new Date().toISOString(); // Current datetime in ISO format
+
+      const payload = {
+        title,
+        content,
+        time,
+      };
+
+      //console.log(payload)
+    
+      const response = await fetch('http://172.26.87.217:8080/message/add', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload)
+      });
+      //console.log('Response:', response.json);
+      setTextboxInput(''); // Clear the input after submission
+      setTextboxExpanded(false); // Optionally collapse after submission
+      updateMessages();
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<any>) => {
+    setTextboxInput(e.target.value)
+  };
+
+  useEffect(()=>{
+    const value = textboxInput;
+    const emojiRegex = /^([\p{Emoji}\s]+)$/u;
+
+    if (emojiRegex.test(value)) {
+      if (!textboxValid) setTextboxValid(true)
+    } else {
+      if (textboxValid) setTextboxValid(false)
+    }
+  }, [textboxInput])
+
+  const convertNumberToEmoji = (number: string) => {
+    const digits = String(number).split('');
+    
+    // Map each digit to its emoji representation
+    const emojiDigits = digits.map(digit => {
+        return String.fromCodePoint(0x0030 + parseInt(digit)) + "\uFE0F\u20E3"; // Adding the emoji variation and combining character
+    });
+    
+    // Join the emoji digits back into a single string
+    return emojiDigits.join('');
+  }
+
+  const loadComments = (id: number) => {
+    fetch('http://172.26.87.217:8080/message/top?parentId='+id)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setComments(data);
+      });
+  }
+
+  const toggleComments = (id: number) => {
+    if (showComments == id) {
+      setCommentInput("");
+      setShowComments(null);
+      setComments(null);
+    } else {
+      setCommentInput("");
+      setShowComments(id);
+      loadComments(id);
+    }
+  }
+
+  const handleSubmitComment  = async (e: React.ChangeEvent<any>) => {
+    e.preventDefault();
+    try {
+      if (showComments == null) return;
+
+      const lines = commentInput.split('\n');
+      const title = lines[0]; // First line as title
+      const content = lines.slice(1).join('\n'); // Rest as contents
+      const time = new Date().toISOString(); // Current datetime in ISO format
+      const parentId = showComments
+
+      const payload = {
+        title,
+        content,
+        time,
+        parentId
+      };
+
+      //console.log(payload)
+    
+      const response = await fetch('http://172.26.87.217:8080/message/add', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload)
+      });
+      //console.log('Response:', response.json);
+      setTextboxInput(''); // Clear the input after submission
+      setTextboxExpanded(false); // Optionally collapse after submission
+      //updateMessages();
+      loadComments(showComments);
+      data.find(i => i.id == showComments).totalChildrenCount += 1;
+    } catch (error) {
+      //console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)] h-screen overflow-hidden">
+      <main className="flex flex-col items-center">
+        <div className="p-3">
+          💬
+        </div>
+        <div className="h-screen rounded-lg border border-gray-700 overflow-hidden">
+          <div className="h-full flex flex-col w-100 max-w-[500px] bg-white dark:bg-[#181818] divide-y divide-gray-700 overflow-y-scroll">
+            {
+              !loading &&
+              data.map((e) => (
+                <div key={e.id} className="px-3 py-1">
+                  <p>{e.title}</p>
+                  <p>{e.content}</p>
+                  <p onClick={()=>{toggleComments(e.id)}}>💬{convertNumberToEmoji(e.totalChildrenCount)}</p>
+                  <div
+                      className={`overflow-hidden transition-all duration-300 ease-in-out ${
+                        showComments == e.id ? 'max-h-full opacity-100' : 'w-0 max-h-0 opacity-0'
+                      }`}
+                    >
+                      {
+                        comments == null &&
+                        <div>⏳</div>
+                      }
+                      {
+                        comments != null &&
+                        <div className="px-3 py-1">
+                          <form id="comment-form" onSubmit={handleSubmitComment} className="flex">
+                            <input
+                              value={commentInput}
+                              onChange={(e) => setCommentInput(e.target.value)}
+                              className="h-auto grow ps-1 me-2 border border-gray-300 rounded-lg"
+                              placeholder="🗣️🗣️🗣️"
+                              required
+                              onInvalid={e => (e.target as HTMLInputElement).setCustomValidity('‼️')}
+                              onInput={e => (e.target as HTMLInputElement).setCustomValidity('')}
+                            />
+                            
+                            <button
+                              form="comment-form"
+                              type="submit"
+                              className={`bg-green-500 text-white rounded-lg focus:outline-none rounded-lg bg-blue-500 text-white p-1`}
+                            >
+                              📢
+                            </button>
+                          </form>
+                          {
+                            comments.map((e) => (
+                              <div key={e.id} className="mt-2">
+                                <p>{e.title}</p>
+                                <p>{e.content}</p>
+                              </div>
+                            ))
+                          }
+                        </div>
+                      }
+                  </div>
+                </div>
+              ))
+            }
+            <div>
+              <br/><br/><br/><br/>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <footer className="fixed bottom-0 right-0 mb-6 me-6 flex flex-col flex-wrap items-center justify-end
+        bg-white dark:bg-[#181818] rounded-lg border border-gray-700">
+
+        <button
+            onClick={handleExpandTextBox}
+            className={`rounded-lg bg-blue-500 text-white p-2 transition duration-300 ${
+              textboxExpanded ? 'hidden' : ''
+            }`}
+          >
+          ✍️
+        </button>
+
+        <div
+          className={`overflow-hidden transition-all duration-300 ease-in-out ${
+            textboxExpanded ? 'max-h-40 opacity-100' : 'w-0 max-h-0 opacity-0'
+          }`}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          <form id="text-form" onSubmit={handleSubmit} className="">
+            <textarea
+              value={textboxInput}
+              style={{resize: "none"}}
+              onChange={handleChange}
+              className="h-auto w-full p-2 rounded-lg accent-transparen focus:outline-hidden"
+              placeholder="🗣️🗣️🗣️"
+              required
+              //onInvalid={e => (e.target as HTMLInputElement).setCustomValidity('‼️')}
+              onInput={
+                e => {
+                  const value = textboxInput;
+                  const emojiRegex = /[\p{Emoji}]/u;
+                  if (emojiRegex.test(value) || value === '') {
+                    (e.target as HTMLInputElement).setCustomValidity('')
+                  } else {
+                    (e.target as HTMLInputElement).setCustomValidity('🚫🔤')
+                    return;
+                  }
+                }
+              }
+            />
+            
+          </form>
+        </div>
+
+        <div className={`flex w-full justify-between p-2 ${ textboxExpanded ? '' : 'hidden' }`}>
+
+          <div className="flex">
+            <button
+              onClick={handleExpandTextBox}
+              className={`rounded-lg transition duration-300 ${
+                textboxExpanded ? '' : 'hidden'
+              }`}
+            >
+              🧹
+            </button>
+
+            <div className={`ms-2 transition duration-500 ${ textboxValid ? 'opacity-0' : 'opacity-100' }`}>
+              🚫🔤
+            </div>
+
+          </div>
+
+
+          <div className="flex">
+            <button
+              form="text-form"
+              type="submit"
+              className={`bg-transparent cursor-pointer rounded-lg focus:outline-none rounded-lg text-white transition duration-300 ${
+                textboxExpanded ? '' : 'hidden'
+              }`}
+            >
+              🤖
+            </button>
+            <button
+              form="text-form"
+              type="submit"
+              className={`bg-transparent cursor-pointer text-white rounded-lg focus:outline-none rounded-lg text-white transition duration-300 ms-2 ${
+                textboxExpanded ? '' : 'hidden'
+              }`}
+            >
+              📢
+            </button>
+          </div>
+        </div>
+        
+        
       </footer>
     </div>
   );
